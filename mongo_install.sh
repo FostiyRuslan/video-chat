2d61af8df0d9a1f623c2e482edf685fbc07c355a
rfos <fostiiruslan2706@gmail.com>
2015-03-31 22:02:34 +0300

    update installation script for mongodb

--- mongo_install.sh
+++ mongo_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-sudo apt-get update
-sudo apt-get install mongodb-org
-sudo service mongod start
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+apt-get update
+apt-get install mongodb-org
+service mongod start
 ps -ef | grep mongo
