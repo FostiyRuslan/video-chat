2124f1e1b765ee40cf219254f351617e889a03cc
rfos <fostiiruslan2706@gmail.com>
2015-04-01 01:05:57 +0300

    add turn server

--- public/js/Utils/PeerConnection.js
+++ public/js/Utils/PeerConnection.js
@@ -9,6 +9,8 @@ var PeerConnection = function(communicator) {
         },
         ice = {
             iceServers: [
+                {url: "stun:global.stun.twilio.com:3478?transport=udp" },
+                {url: 'stun:stun.anyfirewall.com:3478' },
                 {url: 'turn:homeo@turn.bistri.com:80'}
             ]
         },
