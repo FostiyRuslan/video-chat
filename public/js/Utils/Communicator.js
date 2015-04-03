e466d7006a18f706682e2301f2cf774b82f6c997
rfos <fostiiruslan2706@gmail.com>
2015-04-03 08:58:14 +0300

    add styles for messages

--- public/js/Utils/Communicator.js
+++ public/js/Utils/Communicator.js
@@ -1,6 +1,7 @@
 var Communicator = (function (io) {
     var communicator = null;
     var production = 'https://secret-anchorage-6322.herokuapp.com/';
+    var local = 'http://localhost:3000';
 
     function createCommunicator(id) {
         var socket = io.connect(production + id);
