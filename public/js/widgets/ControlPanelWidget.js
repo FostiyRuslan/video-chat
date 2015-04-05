a51eced2e6ae9914260ee943a9e68493e03d4d37
rfos <fostiiruslan2706@gmail.com>
2015-04-05 23:26:33 +0300

    added zeroclipboard

--- public/js/widgets/ControlPanelWidget.js
+++ public/js/widgets/ControlPanelWidget.js
@@ -1,19 +1,35 @@
 var ControlPanelWidget = function () {
-    $('.audio-off').on('click', function() {
-        var video = document.getElementById('local-stream');
-        video.muted = true;
-        $(this).hide();
-        $('.audio-on').show();
-    });
+    function attachEvents() {
+        $('.audio-off').on('click', function() {
+            var video = document.getElementById('local-stream');
+            video.muted = true;
+            $(this).hide();
+            $('.audio-on').show();
+        });
 
-    $('.audio-on').on('click', function() {
-        var video = document.getElementById('local-stream');
-        video.muted = false;
-        $(this).hide();
-        $('.audio-off').show();
-    });
+        $('.audio-on').on('click', function() {
+            var video = document.getElementById('local-stream');
+            video.muted = false;
+            $(this).hide();
+            $('.audio-off').show();
+        });
 
-    $('.show-chat').on('click', function () {
-        $('.chat-container').toggle();
-    });
+        $('.show-chat').on('click', function () {
+            $('.chat-container').toggle();
+        });
+    }
+
+    function initZeroClipboard() {
+        $("#copy-link").attr('data-clipboard-text', location.href);
+        new ZeroClipboard( document.getElementById("copy-link") );
+    }
+
+    function init() {
+        attachEvents();
+        initZeroClipboard();
+    }
+
+    init();
 };
+
+ControlPanelWidget.prototype = new EventEmitter();
